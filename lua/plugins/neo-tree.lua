ae2d5b4b967d453f7237adf7d76e0cff58c670c5
Aleš <ales.pecnik@air-tv.net>
2024-07-22 09:41:56 +0200

    show hidden files

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -8,6 +8,14 @@ return {
         -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
     },
     config = function()
+        require("neo-tree").setup({
+            filesystem = {
+                filtered_items = {
+                    visible = true
+                }
+            }
+        })
+
         vim.keymap.set('n', '<leader>n', ':Neotree selector<CR>', {})
         vim.keymap.set('n', '<C-n>', ':Neotree close<CR>', {})
     end
