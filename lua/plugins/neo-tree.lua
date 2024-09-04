0c40ad40d7e642f02fbbd847078f6b94db3c68a1
Aleš <ales.pecnik@air-tv.net>
2024-09-04 06:35:32 +0200

    follow buffers in treeO

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -9,11 +9,16 @@ return {
     },
     config = function()
         require("neo-tree").setup({
+            close_if_last_window = true,
+            buffers = {
+                follow_current_file = true,
+            },
             filesystem = {
+                follow_current_file = true,
                 filtered_items = {
                     visible = true
-                }
-            }
+                },
+            },
         })
     end
 }
